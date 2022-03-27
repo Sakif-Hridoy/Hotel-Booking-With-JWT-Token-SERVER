66b5333e6e96e8c1f70fe360df357954ae6f2575
Sakif <sakifworkspace@gmail.com>
2022-03-27 15:02:00 +0600

    message

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = 5000;
 
+const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://mydbuser1:jmXHknmahHGBXCXq@cluster0.djg6r.mongodb.net/burjAlArab?retryWrites=true&w=majority";
@@ -11,7 +14,11 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const collection = client.db("burjAlArab").collection("bookings");
 
-  console.log("database connected")
+  console.log("database connected");
+  app.post('/addBooking',(req,res)=>{
+      const newBooking = req.body;
+      console.log(newBooking)
+  })
   // perform actions on the collection object
 //   client.close();
 });
